268865e1b9a7f356f27419b5adbf8f6acc93635b
Saleumsack <saleumsack16@gmail.com>
2018-10-24 20:08:44 +0700

    Connecting to MongoDB with Mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://saleumsack:ihave4dick@ds139883.mlab.com:39883/thebrown-connector"
+}
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log("Connected to MonGoDB"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   console.log("<h1>Hello World Mother fuck u!</h1>");
@@ -9,5 +19,6 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => { console.log(`Listen add port: ${port}`)});
-
+app.listen(port, () => {
+  console.log(`Listen add port: ${port}`);
+});
